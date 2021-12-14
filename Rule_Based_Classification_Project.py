ac7e38dd5db6d7cca46502fdb1387a9a60744407
Kübra Bilinmiş <81189969+Kubrabilinmis@users.noreply.github.com>
2021-12-15 02:04:26 +0300

    Update Rule_Based_Classification_Project.py

--- Rule_Based_Classification_Project.py
+++ Rule_Based_Classification_Project.py
@@ -88,69 +88,41 @@ df.groupby(["COUNTRY","SOURCE"]).agg({"PRICE": "mean" })
 df.groupby(["COUNTRY","SOURCE","SEX","AGE"]).agg({"PRICE" : "mean"})
 
 
+
 # GOREV 3 :
 # Çıktıyı PRICE’a göre sıralayınız.
-# Önceki sorudaki çıktıyı daha iyi görebilmek için sort_values metodunu azalan olacak şekilde PRICE’a göre uygulayınız.
-# Çıktıyı agg_df olarak kaydediniz.
-
-agg_df = df.groupby(["COUNTRY","SOURCE","SEX","AGE"]).agg({"PRICE" : "sum"}).sort_values(by='PRICE',ascending=False)
+agg_df = df.groupby(["COUNTRY","SOURCE","SEX","AGE"]).agg({"PRICE" : "mean"}).sort_values(by='PRICE',ascending=False)
 agg_df.head()
+
 
 
 # GOREV 4 :
 # Index’te yer alan isimleri değişken ismine çeviriniz.
-# Üçüncü sorunun çıktısında yer alan price dışındaki tüm değişkenler index isimleridir.
-# Bu isimleri değişken isimlerine çeviriniz.
-
 agg_df = agg_df.reset_index()
-agg_df
+
 
 
 # GOREV 5 :
-# age değişkenini kategorik değişkene çeviriniz ve
-# agg_df’e ekleyiniz.
-# Age sayısal değişkenini kategorik değişkene çeviriniz.
-# Aralıkları ikna edici şekilde oluşturunuz.
-# Örneğin: ‘0_18', ‘19_23', '24_30', '31_40', '41_70'
-
+# age değişkenini kategorik değişkene çeviriniz ve  agg_df’e ekleyiniz.
 df["AGE"].nunique()
 #46
-agg_df["AGE_CAT"] = pd.cut(agg_df["AGE"] , bins = [0, 18, 23, 30, 40, 66], labels = ["0_18", "19_23", "24_30", "31_40", "41_66"])
-
+agg_df["AGE_CAT"] = pd.cut(agg_df["AGE"] , bins = [0, 18, 23, 30, 40, agg_df["AGE].max()], labels = ["0_18", "19_23", "24_30", "31_40", "41_"+str(agg_df["AGE"].max())])
 agg_df.head()
 
-
-
+                                                                             
 # GOREV 6 :
 # Yeni seviye tabanlı müşterileri (persona) tanımlayınız.
-# Yeni seviye tabanlı müşterileri (persona) tanımlayınız ve veri setine değişken olarak ekleyiniz.
-# Yeni eklenecek değişkenin adı: customers_level_based
-# Önceki soruda elde edeceğiniz çıktıdaki gözlemleri bir araya getirerek customers_level_based
-# değişkenini oluşturmanız gerekmektedir.
-
 agg_df.columns
-
 agg_df["customers_level_based"] = [col[0].upper() + "_" + col[1].upper() + "_" + col[2].upper() + "_" + col[5].upper() for col in agg_df.values]
-
 agg_df = agg_df[["customers_level_based", "PRICE"]]
 agg_df.head()
-
 agg_df["customers_level_based"].value_counts()
-
 agg_df = agg_df.groupby("customers_level_based").agg({"PRICE": "mean"})
-
 agg_df = agg_df.reset_index()
-agg_df
-
 
 
 # GOREV 7 :
 # Yeni müşterileri (personaları) segmentlere ayırınız.
-# Yeni müşterileri (Örnek: USA_ANDROID_MALE_0_18) PRICE’a göre 4 segmente ayırınız.
-# Segmentleri SEGMENT isimlendirmesi ile değişken olarak agg_df’e ekleyiniz.
-# Segmentleri betimleyiniz (Segmentlere göre group by yapıp price mean, max, sum’larını alınız).
-# C segmentini analiz ediniz (Veri setinden sadece C segmentini çekip analiz ediniz).
-
 agg_df["SEGMENT"] = pd.qcut(agg_df["PRICE"] , 4 , labels = ["D","C","B","A"])
 agg_df.groupby("SEGMENT").agg({"PRICE": ["mean","max","sum"]})
 agg_df[agg_df["SEGMENT"] == "C"]
@@ -158,14 +130,8 @@ agg_df[agg_df["SEGMENT"] == "C"]
 
 # GOREV 8 :
 # Yeni gelen müşterileri segmentlerine göre sınıflandırınız ve ne kadar gelir getirebileceğini tahmin ediniz.
-# 33 yaşında ANDROID kullanan bir Türk kadını hangi segmente aittir ve ortalama ne kadar gelir kazandırması beklenir?
-# 35 yaşında IOS kullanan bir Fransız kadını hangi segmente ve ortalama ne kadar gelir kazandırması beklenir?
-
 new_user = "TUR_ANDROID_FEMALE_31_40"
 new_user2 = "FRA_IOS_FEMALE_31_40"
 agg_df[agg_df["customers_level_based"] == new_user]
 agg_df[agg_df["customers_level_based"] == new_user2]
-
-
-
 
